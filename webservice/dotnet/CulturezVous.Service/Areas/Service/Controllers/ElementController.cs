40917ccdeb1fe4f05bc6cdb1b4668703c404cb79
Damien Mayance <valryon@gmail.com>
2012-11-12 18:19:56 +0100

    Starting Webservice with ASP.NET MVC
    Just a start.

---
+++ webservice/dotnet/CulturezVous.Service/Areas/Service/Controllers/ElementController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CulturezVous.Service.Areas.Service.Controllers
+{
+    public class ElementController : ServiceBaseController
+    {
+        [HttpPost]
+        public ActionResult Index(string format)
+        {
+            return PrepareResponse(format, "truc");
+        }
+
+    }
+}
---
+++ webservice/dotnet/CulturezVous.Service/Areas/Service/Controllers/ServiceBaseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CulturezVous.Service.Areas.Service.Controllers
+{
+    public abstract class ServiceBaseController : Controller
+    {
+        public ActionResult PrepareResponse(string format, object model)
+        {
+            if (format == "xml")
+            {
+                return new ContentResult
+                {
+                    Content = "<truc></truc>",
+                    ContentType = "application/xml"
+                };
+            }
+            else if (format == "json")
+            {
+                return new JsonResult()
+                {
+                    Data = "{Pouet pouet}",
+                };
+            }
+            else throw new ArgumentException("Unknow format " + format);
+        }
+
+    }
+}
